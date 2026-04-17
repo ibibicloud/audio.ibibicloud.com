24ce5cdb104d6b2bcc510bde43ce028e0ff7aa2e
小乙哥 <ibibicloud@163.com>
2026-04-18 01:34:38 +0800

    日常维护

--- functions/api/douyin.creator.audio.js
+++ functions/api/douyin.creator.audio.js
@@ -2,10 +2,10 @@
 import { HttpClient } from '../utils/HttpClient.js';
 
 export async function onRequestGet(context) {
-	const url = new URL(context.request.url);
+    const url = new URL(context.request.url);
 
     const typeData = {
-    	'推荐': { type: 'recommend', category_id: '1' },
+        '推荐': { type: 'recommend', category_id: '1' },
         '热门榜': { type: 'rank', category_id: '7088298745502646280' },
         // '收藏': { type: 'fav', category_id: '1' },
         '飙升榜': { type: 'rank', category_id: '708297994563059748' },
@@ -16,7 +16,7 @@ export async function onRequestGet(context) {
         'DJ': { type: 'category', category_id: '7395861511152864050' },
         '搞笑': { type: 'category', category_id: '7397653031963167526' },
         '流行': { type: 'category', category_id: '7397326893978405683' },
-        '伤感': { type: 'category', category_id: '7397328346998213386' },
+        '伤感': { type: 'category', category_id: '7397328346998213386' }
     };
 
     const type = url.searchParams.get('type') || '推荐';
@@ -25,10 +25,10 @@ export async function onRequestGet(context) {
     const currentType = typeData[type] || typeData['推荐'];
 
     const queryData = {
-        type: currentType.type
-        ,category_id: currentType.category_id
-        ,cursor: (page - 1) * 20
-        ,count: 20
+        type: currentType.type,
+        category_id: currentType.category_id,
+        cursor: (page - 1) * 20,
+        count: 20
     };
 
     const result = await HttpClient.get('https://creator.douyin.com/web/api/media/music/list', queryData);
