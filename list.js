7bd1e0b87e347cb55ed9e5ac29ef6071409b9cf2
小乙哥 <ibibicloud@163.com>
2026-04-17 08:34:25 +0800

    更新 list.js

--- list.js
+++ list.js
@@ -1,24 +1,22 @@
-// @charset "UTF-8";
-
 var songList = [
-	{title: '演示专用音频', src: 'https://audio.ibibicloud.com/list/demo.m4a'},
-	{title: '@旁觀者.创作的原声', src: 'https://lf9-music-east.douyinstatic.com/obj/ies-music-hj/7566809028706208570.mp3'},
-	{title: '@大虎创', src: 'https://sf6-cdn-tos.douyinstatic.com/obj/ies-music/7287582969808407351.mp3'},
-	{title: '@真相创作的原声', src: 'https://sf6-cdn-tos.douyinstatic.com/obj/ies-music/7568413551967111986.mp3'},
-	{title: '@y哥在美国创作的原声', src: 'https://sf6-cdn-tos.douyinstatic.com/obj/ies-music/7511503698674191167.mp3'},
-	{title: '时尚走秀（纯音乐）', src: 'https://sf5-hl-ali-cdn-tos.douyinstatic.com/obj/tos-cn-ve-2774/3af535b4e447447e94542a5f2882bf4f'},
-	{title: 'Normally(MIX)', src: 'https://sf5-hl-ali-cdn-tos.douyinstatic.com/obj/tos-cn-ve-2774/o8ZarbFeXIDUXoQFCpEAfeEoP4M1zQMBGDJIgC'},
-	{title: '@阿崎创作的原声', src: 'https://lf9-music-east.douyinstatic.com/obj/ies-music-hj/7555355225226332988.mp3'},
-	{title: '@鉄男创作的原声', src: 'https://sf5-hl-ali-cdn-tos.douyinstatic.com/obj/ies-music/7132028240119139086.mp3'},
-	{title: '杀死比尔', src: 'https://sf6-cdn-tos.douyinstatic.com/obj/tos-cn-ve-2774/oMXaQAQmmFciFCEKvRffzhACHEIpfBZDMAuQO3'},
-	{title: '眼睛里只有胜负', src: 'https://sf6-cdn-tos.douyinstatic.com/obj/tos-cn-ve-2774/ocob19vCM9gIByKtIfEuwBCQQZikdQf4hg2Boi'},
-	{title: '@无声仿有声创作的原声', src: 'https://lf9-music-east.douyinstatic.com/obj/ies-music-hj/7546950245591042876.mp3'},
-	{title: '@KKK🇭🇰创作的原声', src: 'https://sf5-hl-ali-cdn-tos.douyinstatic.com/obj/ies-music/7492357577989311243.mp3'},
-	{title: '@阿崎创作的原声', src: 'https://lf9-music-east.douyinstatic.com/obj/ies-music-hj/7554434838284258108.mp3'},
-	{title: '@佩创作的原声', src: 'https://lf9-music-east.douyinstatic.com/obj/ies-music-hj/7549213983308303164.mp3'},
-	{title: '@京剧·吴昊🧚‍♀️ ⁸¹¹创作的原声', src: 'https://sf5-hl-ali-cdn-tos.douyinstatic.com/obj/ies-music/7398108981165624127.mp3'},
-	{title: '@东方笛箫创作的原声', src: 'https://lf9-music-east.douyinstatic.com/obj/ies-music-hj/7447842038453668667.mp3'},
-	{title: '@虎文影视创作的原声', src: 'https://lf26-music-east.douyinstatic.com/obj/ies-music-hj/7525039462993251129.mp3'},
-	{title: 'Everybody\'s Beat - Lupo mix', src: 'https://sf6-cdn-tos.douyinstatic.com/obj/tos-cn-ve-2774/ocaWHRiuGIBAewOByGhwP9ijzyMwsIMAo1XUmA'},
-	{title: '@DJ纹子小店创作的原声', src: 'https://lf9-music-east.douyinstatic.com/obj/ies-music-hj/7472472422004968252.mp3'}
+    {title: "演示音频专用", src: "https://audio.ibibicloud.com/list/demo.m4a"}
+    ,{title: "@旁觀者.创作的原声", src: "https://lf26-music-east.douyinstatic.com/obj/ies-music-hj/7566809028706208570.mp3"}
+    ,{title: "@大虎创作的原声", src: "https://sf5-hl-ali-cdn-tos.douyinstatic.com/obj/ies-music/7287582969808407351.mp3"}
+    ,{title: "@真相创作的原声", src: "https://sf6-cdn-tos.douyinstatic.com/obj/ies-music/7568413551967111986.mp3"}
+    ,{title: "@y哥在美国创作的原声", src: "https://sf5-hl-ali-cdn-tos.douyinstatic.com/obj/ies-music/7511503698674191167.mp3"}
+    ,{title: "时尚走秀（纯音乐）", src: "https://sf5-hl-ali-cdn-tos.douyinstatic.com/obj/tos-cn-ve-2774/3af535b4e447447e94542a5f2882bf4f"}
+    ,{title: "Normally(MIX)", src: "https://sf6-cdn-tos.douyinstatic.com/obj/tos-cn-ve-2774/o8ZarbFeXIDUXoQFCpEAfeEoP4M1zQMBGDJIgC"}
+    ,{title: "@阿崎创作的原声", src: "https://lf9-music-east.douyinstatic.com/obj/ies-music-hj/7555355225226332988.mp3"}
+    ,{title: "@鉄男创作的原声", src: "https://sf5-hl-ali-cdn-tos.douyinstatic.com/obj/ies-music/7132028240119139086.mp3"}
+    ,{title: "杀死比尔", src: "https://sf5-hl-ali-cdn-tos.douyinstatic.com/obj/tos-cn-ve-2774/oMXaQAQmmFciFCEKvRffzhACHEIpfBZDMAuQO3"}
+    ,{title: "眼睛里只有胜负", src: "https://sf6-cdn-tos.douyinstatic.com/obj/tos-cn-ve-2774/ocob19vCM9gIByKtIfEuwBCQQZikdQf4hg2Boi"}
+    ,{title: "@无声仿有声创作的原声", src: "https://lf26-music-east.douyinstatic.com/obj/ies-music-hj/7546950245591042876.mp3"}
+    ,{title: "@KKK🇭🇰创作的原声", src: "https://sf6-cdn-tos.douyinstatic.com/obj/ies-music/7492357577989311243.mp3"}
+    ,{title: "@阿崎创作的原声", src: "https://lf26-music-east.douyinstatic.com/obj/ies-music-hj/7554434838284258108.mp3"}
+    ,{title: "@佩创作的原声", src: "https://lf26-music-east.douyinstatic.com/obj/ies-music-hj/7549213983308303164.mp3"}
+    ,{title: "@京剧·吴昊🧚‍♀️ ⁸¹¹创作的原声", src: "https://sf6-cdn-tos.douyinstatic.com/obj/ies-music/7398108981165624127.mp3"}
+    ,{title: "@东方笛箫创作的原声", src: "https://lf26-music-east.douyinstatic.com/obj/ies-music-hj/7447842038453668667.mp3"}
+    ,{title: "@虎文影视创作的原声", src: "https://lf9-music-east.douyinstatic.com/obj/ies-music-hj/7525039462993251129.mp3"}
+    ,{title: "Everybody's Beat - Lupo mix", src: "https://sf6-cdn-tos.douyinstatic.com/obj/tos-cn-ve-2774/ocaWHRiuGIBAewOByGhwP9ijzyMwsIMAo1XUmA"}
+    ,{title: "@DJ纹子小店创作的原声", src: "https://lf9-music-east.douyinstatic.com/obj/ies-music-hj/7472472422004968252.mp3"}
 ];
